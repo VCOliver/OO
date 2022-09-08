f72f65b4323e9862851aae6e303ff0e232b60fff
Victor Cruz <92812122+VCOliver@users.noreply.github.com>
2022-09-08 11:06:23 -0300

    Fixed database problem

--- src/models/Muscles.java
+++ src/models/Muscles.java
@@ -4,34 +4,42 @@ package models;
  * Enumeracao para padronizar os tipos de musculos ou grupamentos de musculos.
  * 
  * @author VCOliver
+ * @version 1.2
  *
  */
 public enum Muscles {
-
-	BICEPS(0), 
-	TRICEPS(1), 
-	COSTAS(2), 
-	PEITO(3), 
-	OMBROS(4), 
-	PERNAS(5), 
-	PANTURRILHA(6), 
-	PESCOCO(7);
+	
+	BICEPS(0, "Biceps"), 
+	TRICEPS(1, "Triceps"), 
+	COSTAS(2, "Costas"), 
+	PEITO(3, "Peito"), 
+	OMBROS(4, "Ombros"), 
+	PERNAS(5, "Pernas"), 
+	PANTURRILHA(6, "Panturrilha"), 
+	PESCOCO(7, "Pescoco");
 	
 	private final int index;
+	private final String value;
 	
 	/**
-	 * Enum constructor para criar relacao index e valor
-	 * 
-	 * @param int index 
-	 * 
+	 * Enum constructor para criar relação valor, index e enum
 	 */
-	
-	Muscles (int index){
+	Muscles(int index, String value){
 		this.index = index;
+		this.value = value;
+	}
+	
+	/**
+	 * returna o valor do enum
+	 * @return the String value	
+	 */
+	public String getValue() {
+		return value;
 	}
 
 	/**
-	 * @return the index
+	 * retorna o index do enum
+	 * @return the int index
 	 */
 	public int getIndex() {
 		return index;
