9546a194c9d3276192721c53b764772398031a35
Victor Cruz <92812122+VCOliver@users.noreply.github.com>
2022-09-08 00:18:20 -0300

    Create DataBase.java

---
+++ src/models/DataBase.java
@@ -0,0 +1,62 @@
+package models;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Classe utilizada para armazenar todos os bancos de dados
+ * 
+ * @author Victor Cruz de Oliveira
+ * @see Exercise
+ * @see User
+ * @see Admin
+ */
+
+public class DataBase {
+
+	/**
+	 * 2D array para armazerar os exercicios e seus respectivos musculos associados
+	 * @see Exercise
+	 */
+	private static ArrayList<ArrayList<Exercise>> exercises;
+
+	/**
+	 * Array para armazenar usuarios
+	 * @see User
+	 */
+	private static ArrayList<User> users = new ArrayList<>();
+	
+	/**
+	 * Array para armazenar admins
+	 * @see Admin
+	 */
+	private static ArrayList<Admin> admins = new ArrayList<>();
+
+	
+	// Getters
+	
+	/**
+	 * Returna Array 2D de exercicios 
+	 * @return the exercises
+	 */
+	public static ArrayList<ArrayList<Exercise>> getExercises() {
+		return exercises;
+	}
+
+	/**
+	 * Retorna lista de usuarios
+	 * @return the users
+	 */
+	public static ArrayList<User> getUsers() {
+		return users;
+	}
+
+	/**
+	 * Retorna lista de admins
+	 * @return the admins
+	 */
+	public static ArrayList<Admin> getAdmins() {
+		return admins;
+	}
+	
+}
