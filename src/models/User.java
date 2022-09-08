b70b7991324dd9a23c242207ba694fb0fa54241f
Victor Cruz <92812122+VCOliver@users.noreply.github.com>
2022-09-08 00:20:20 -0300

    Update and rename src/User.java to src/models/User.java

---
+++ src/models/User.java
@@ -0,0 +1,78 @@
+package models;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Classe para definir usuarios padroes
+ * @author VCOliver
+ * @version 1.1
+ */
+
+public class User extends Person{
+	
+	//Attributes
+	List<Routine> routines = new ArrayList<>();
+	
+	//Constructor
+	public User(String name, String password, String email) {
+		user_name = name;
+		this.password = password;
+		this.email = email;
+	}
+	
+	
+	//Getters
+	
+	/**
+	 * Returna o nome do usuario
+	 * @return
+	 */
+	String getName() {
+		return this.user_name;
+	}
+	
+	/**
+	 * retorna o email do usuario
+	 * @return
+	 */
+	String getEmail() {
+		return this.email;
+	}
+	
+	/** Retorna lista de rotinas do usuario
+	 * @return the routines
+	 */
+	public List<Routine> getRoutines() {
+		return routines;
+	}
+	
+	
+	//Methods
+
+	/**
+	 * Cria nova rotina para o usuario
+	 * @param name
+	 */
+	void createRoutine(String name) {
+		
+		Routine routine = new Routine(name, user_name);
+		
+		this.routines.add(routine); // Adds routine to User
+		
+	}
+
+	void deleteRoutine(Routine routine) {
+		this.routines.remove(routine);
+	}
+
+	
+	@Override
+	void deleteAccount() {
+		
+		if(DataBase.getUsers().contains(User.this)) {
+			DataBase.getUsers().remove(User.this);
+		}
+
+	}	
+	
+}
