ea4e18335dc6cde19c5231ed06de0220b805734d
Victor Cruz <92812122+VCOliver@users.noreply.github.com>
2022-09-09 11:54:42 -0300

    Started controllers

---
+++ src/controllers/ExerciseController.java
@@ -0,0 +1,28 @@
+package controllers;
+
+import java.util.ArrayList;
+import models.DataBase;
+import models.Muscles;
+
+/**
+ * Controla o banco de dados dos exercicios e com a criacao dos exercicios
+ * @author VCOliver
+ */
+public class ExerciseController {
+	
+	/**
+	 * Initializes exercises database if null
+	 */
+	public static void InitExerciseDataBase(){
+		
+		if(DataBase.getExercises().isEmpty()) {
+			for(Muscles muscle : Muscles.values()) {
+				DataBase.getExercises().put(muscle.getValue(), new ArrayList<>());
+			}
+		}
+		
+		System.out.println(DataBase.getExercises());
+		
+	}
+	
+}
