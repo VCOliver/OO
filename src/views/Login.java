5e3c4481f2f40bf868c88b5b42bb99e6d43f82cf
Victor Cruz <92812122+VCOliver@users.noreply.github.com>
2022-09-09 17:51:42 -0300

    Login page finished

--- src/views/Login.java
+++ src/views/Login.java
@@ -1,78 +1,129 @@
 /**
  * 
  */
-package view;
+package views;
 
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.*;
 
 /**
+ * Primeira tela a ser vista pelo usuario, responsavel pelo login
  * @author VCOliver
- *
  */
-public class Login implements ActionListener {
+@SuppressWarnings("serial")
+public class Login extends JFrame implements KeyListener{
 
+	//Attributes
 	private static JFrame frame;
 	private static JButton btn;
-	private static JTextField inputEmail;
-	private static JPasswordField inputSenha;
-	private static JLabel inserirSenha;
-	private static JLabel inserirEmail;
-	private static JLabel criarConta;
+	private static JTextField emailInput;
+	private static JPasswordField passwordInput;
+	private static JLabel passwordLabel;
+	private static JLabel emailLabel;
+	private static JLabel createAccount;
 	
 	public Login() {
 		
-		frame = new JFrame("Cadastro");
-		btn = new JButton("Logar");
-		inputEmail = new JTextField(10);
-		inputSenha = new JPasswordField(10);
-		inserirEmail = new JLabel("Insira seu email:");
-		inserirSenha = new JLabel("Insira sua senha:");
-		criarConta = new JLabel("Não tem conta? Cadastre-se!");
 		
-		frame.getContentPane().setBackground(Color.getHSBColor(217, 228, 241));
-		frame.setTitle("Cadastro");
+		//Setting up frame
+		frame = new JFrame("Login");
+		frame.getContentPane().setBackground(new Color(17, 192, 102));
+		frame.setTitle("Login");
 		frame.setSize(500, 500);
-		frame.setLocation(500, 300);
+		frame.setResizable(true);
+		frame.setLocationRelativeTo(null); // Centers frame on the screen
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(null);
 		
-		inputEmail.setBounds(100, 130, 300, 30);
+		//Setting up email box
+		emailLabel = new JLabel("Insira seu email:");
+		emailLabel.setFont(new Font("Arial", Font.BOLD, 16));
+		emailLabel.setBounds(100, 90, 300, 30);
+		emailInput = new JTextField(10);
+		emailInput.setBounds(100, 130, 300, 30);
+		emailInput.setFocusable(true);
 		
-		inserirEmail.setFont(new Font("Arial", Font.BOLD, 16));
-		inserirEmail.setBounds(100, 90, 300, 30);
+		//Setting up password box
+		passwordLabel = new JLabel("Insira sua senha:");
+		passwordLabel.setFont(new Font("Arial", Font.BOLD, 16));
+		passwordLabel.setBounds(100, 170, 300, 30);
+		passwordInput = new JPasswordField(10);
+		passwordInput.setBounds(100, 210, 300, 30);
 		
-		inputSenha.setBounds(100, 210, 300, 30);
-		
-		inserirSenha.setFont(new Font("Arial", Font.BOLD, 16));
-		inserirSenha.setBounds(100, 170, 300, 30);
-		
+		//Setting up button
+		btn = new JButton("Login");
 		btn.setBounds(190,320,120,30);
 		
-		criarConta.setFont(new Font("Arial", Font.BOLD, 14));
-		criarConta.setForeground(Color.getHSBColor(226, 40, 27));
-		criarConta.setBounds(130, 350, 300, 30);
+		//Setting up Create account box
+		createAccount = new JLabel("Crie sua conta aqui!");
+		createAccount.setFont(new Font("Arial", Font.BOLD, 14));
+		createAccount.setForeground(Color.RED);
+		createAccount.setBounds(180, 350, 300, 30);
+		createAccount.setCursor(new Cursor(Cursor.HAND_CURSOR));
 		
-		frame.add(inserirEmail);
-		frame.add(inputEmail);
-		frame.add(inserirSenha);
-		frame.add(inputSenha);
+		//Adding setups to frame
+		frame.add(emailLabel);
+		frame.add(emailInput);
+		frame.add(passwordLabel);
+		frame.add(passwordInput);
 		frame.add(btn);
-		frame.add(criarConta);
+		frame.add(createAccount);
+		
+		//Action listeners
+		frame.addKeyListener(this);
+		emailInput.addKeyListener(this);
+		passwordInput.addKeyListener(this);
+		
+		btn.addActionListener(e -> { 		 //Button action listener
+			String email = emailInput.getText();
+			String password = String.valueOf(passwordInput.getPassword());
+			
+			System.out.println(email + " " + password);
+		});
+		
+		createAccount.addMouseListener(new MouseAdapter() {
+		      @Override
+		      public void mouseClicked(MouseEvent e) {
+		    	  System.out.println("Somebody wants to create a new account."); 
+		      }
+		    }
+		);
 		
 		frame.setVisible(true);
 		
-		btn.addActionListener(this);
 	}
-	
+
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getKeyCode() == 10) {
+			
+			String email = emailInput.getText();
+			String password = String.valueOf(passwordInput.getPassword());
+			
+			System.out.println(email + " " + password);
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
